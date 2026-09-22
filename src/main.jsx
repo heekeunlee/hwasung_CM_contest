0f64091e73489cc457d7b14176e2b8abc18d5f69
heekeunlee <clzmyiz@gmail.com>
2026-09-22 20:26:31 +0900

    Shift office visual language to isometric broadcast studio

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './creative-rooms.css'
+import './vector-broadcast.css'
 createRoot(document.getElementById('root')).render(<StrictMode><App /></StrictMode>)
